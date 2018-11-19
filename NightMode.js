e990d739c0b9b715c3b4745766d833b6ee110383
1nternetUser <43967720+1nternetUser@users.noreply.github.com>
2018-11-19 12:17:46 -0800

    Update NightMode.js

--- NightMode.js
+++ NightMode.js
@@ -17,3 +17,28 @@ function NightMode()
     }
 
 }
+
+function apply_theme(theme)
+{
+  var fg_color = '';
+  var bg_color = '';
+  if ( theme == null)
+  {
+    theme = 'light';
+  }
+  if ( theme == 'light')
+  {
+    fg_color = 'black';
+    bg_color = 'white';
+  }
+  else if ( theme == 'dark' )
+  {
+  fg_color = 'white';
+  bg_color = 'navy';
+  }
+  document.body.style.color = fg_color;
+  document.body.stle.backgroundColor = bg_color;
+  localStorage.setItem( 'theme', theme );
+}
+var saved_theme = localStorage.getItem('theme');
+apply_theme(saved_theme);
