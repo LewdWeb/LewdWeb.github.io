7824acd433e349ba4185804b89b00ef96eb70b4a
1nternetUser <43967720+1nternetUser@users.noreply.github.com>
2018-11-19 11:57:43 -0800

    Update NightMode.js

--- NightMode.js
+++ NightMode.js
@@ -1,17 +1,10 @@
-function toggleNightMode() {
+function NightMode() 
+{
   var color = document.getElementById('mydiv').style.color;
     
   var backgroundColor = document.getElementById('mydiv').style.backgroundColor;
   
     
-  if (color == "black" && backgroundColor == "white") {
-        document.getElementById('mydiv').style.color="white";
-        document.getElementById('mydiv').style.backgroundColor="black";
-        
-        
-    } else {
-        document.getElementById('mydiv').style.color="black";
-        document.getElementById('mydiv').style.backgroundColor="white";
-        
-    }
+  if ( backgroundColor == "white" ) { document.getElementById('mydiv').style.backgroundColor="black"; } 
+  else { document.getElementById('mydiv').style.backgroundColor="white"; }
 }
